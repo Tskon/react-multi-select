9cd46dd0961e0b19c7ba160bcf478e331deb84be
Tskon <tsekhmeister.k@gmail.com>
2020-06-16 03:02:26 +0300

    add build dir + custom index

---
+++ build/precache-manifest.a4800a585273f9a8ca82bb3b04f05ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c13fd82e6948ac2d38f771e111f3bd9",
+    "url": "/react-multi-select/index.html"
+  },
+  {
+    "revision": "658b1a154a6e8807a161",
+    "url": "/react-multi-select/static/css/main.8dd5206c.chunk.css"
+  },
+  {
+    "revision": "bded5a6f9185b8625535",
+    "url": "/react-multi-select/static/js/2.856c0737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-multi-select/static/js/2.856c0737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658b1a154a6e8807a161",
+    "url": "/react-multi-select/static/js/main.9208add3.chunk.js"
+  },
+  {
+    "revision": "7c69b09bc97ea4b322ed",
+    "url": "/react-multi-select/static/js/runtime-main.8c42846b.js"
+  }
+]);
